2b17aefe683079bfdba62c6b04a9db34cc08add1
Quanzeng You <REDMOND.quyou@GCRAZGDL123.redmond.corp.microsoft.com>
2017-12-07 00:04:01 +0000

    Add more training scripts for LSTM.

--- model_lstm.py
+++ model_lstm.py
@@ -44,6 +44,37 @@ class SkeletonAction(nn.Module):
         x = x.view(batch_size, seq_len, x.size(1))
         return x
 
+class SkeletonAction_AVG_H(nn.Module):
+    def __init__(self, input_size, hidden_size, num_class, num_layers = 3, use_bias = True, dropout = 0):
+        super(SkeletonAction_AVG_H, self).__init__()
+        self.input_size = input_size
+        self.hidden_isze = hidden_size
+        self.num_class = num_class
+        self.num_layers = num_layers
+        self.use_bias = use_bias
+        self.dropout = dropout
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout = dropout)
+        self.linear = nn.Linear(hidden_size, num_class)
+        self.dropout_layer = nn.Dropout(dropout)
+    def init_weights(self):
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
+    
+    def forward(self, x, mask):
+        '''
+        x: batch x seq_len x input_size
+        '''
+        batch_size = x.size(0)
+        seq_len = x.size(1)
+        x,_ = self.lstm(x)
+        x = x.contiguous()
+        x = x.view((-1, x.size(-1)))
+        #x = self.dropout_layer(x)
+        x = (x * mask).sum(dim = 1) / mask.sum(dim = 1)
+        x = self.linear(x)
+        return x
+
 class SkeletonAction_FT(nn.Module):
     def __init__(self, input_size, hidden_size, num_class, num_layers = 1, use_bias = True, dropout = 0):
         super(SkeletonAction_FT, self).__init__()
---
+++ train_lstm_gaussian_aug_avg.py
@@ -0,0 +1,153 @@
+import argparse
+import torch
+import cPickle as pickle
+import torch.nn as nn
+import numpy as np
+import os
+import glob
+import pdb
+
+#from data_loader_lstm import get_loader 
+from data_loader_lstm_gaussian_aug import get_loader 
+from model_lstm import SkeletonAction_AVG_H as SkeletonAction
+from torch.autograd import Variable 
+import torch.nn.functional as F
+
+import climate
+import logging
+logging = climate.get_logger(__name__)
+climate.enable_default_logging()
+
+
+def main(args):
+    # Build data loader
+    if not os.path.isdir(args.model_path):
+        os.makedirs(args.model_path)
+
+    data_loader,ds_class = get_loader(args.data_dir, args.seq_len, args.batch_size,
+                             shuffle=True, num_workers=args.num_workers, ds = args.ds) 
+
+    # Build eval data loader
+    if hasattr(ds_class, 'lbl2id'):
+        eval_data_loader,_ = get_loader(args.data_dir_test, args.batch_size,
+                             shuffle=True, num_workers=args.num_workers, ds = args.ds, lbl2id = ds_class.lbl2id) 
+    else:
+        eval_data_loader,_ = get_loader(args.data_dir_test, args.batch_size,
+                             shuffle=True, num_workers=args.num_workers, ds = args.ds)
+ 
+    model = SkeletonAction(args.input_size, args.hidden_size, args.num_class, args.num_layers, args.use_bias, args.dropout)
+
+    # Loss and Optimizer
+    criterion = nn.CrossEntropyLoss()
+    if torch.cuda.is_available():
+        model.cuda()
+        criterion = criterion.cuda()
+
+    params = model.parameters()
+    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
+
+    # Load the trained model parameters
+    # Now, we try to find the latest encoder and decoder model.
+    if os.path.isdir(args.model_path) and os.listdir(args.model_path):
+        m_fn = max(glob.glob(os.path.join(args.model_path, 'model*')), key = os.path.getctime)
+        if m_fn:
+            model.load_state_dict(torch.load(m_fn))
+
+    # Train the Models
+    total_step = len(data_loader)
+    for epoch in range(args.num_epochs):
+        total_train = 0
+        total_correct = 0
+        total_train_2 = 0
+        total_correct_2 = 0
+        for i_step, (lbl, data, length) in enumerate(data_loader):
+            # Set mini-batch dataset
+            lbl = Variable(lbl)
+            data = Variable(data)
+            mask = torch.zeros(data.size(0), data.size(1))
+            for i,m in zip(length, mask):
+                m[0:i[0]] = 1
+            mask = Variable(mask)
+            if torch.cuda.is_available():
+                lbl = lbl.cuda()
+                data = data.cuda()
+                mask = mask.cuda()
+            model.zero_grad()
+            mask = mask.unsqueeze(2)
+            opt = model(data, mask)
+            # compute accuracy.        
+            pred_lbl = opt.max(dim = -1)[1].data.cpu()
+            total_train += data.size(0)
+            total_correct += (pred_lbl.squeeze() == lbl.data.cpu().squeeze()).sum()
+            loss = criterion(opt, lbl)
+            loss.backward()
+            optimizer.step()
+            # Eval the trained model
+            if i_step % args.eval_step == 0:
+                model.eval()
+                total_num = 0
+                correct_num = 0
+                correct_num2 = 0
+                for k_step, (lbl, data, length) in enumerate(eval_data_loader):
+                    lbl = Variable(lbl)
+                    data = Variable(data)
+                    mask = torch.zeros(data.size(0), data.size(1))
+                    for i,m in zip(length, mask):
+                        m[0:i[0]] = 1
+                    if torch.cuda.is_available():
+                        lbl = lbl.cuda()
+                        data = data.cuda()
+                        mask = mask.cuda()
+        
+                    mask = Variable(mask)
+                    mask = mask.unsqueeze(2)
+                    model.zero_grad()
+                    opt = model(data, mask)
+                    pred_lbl = opt.max(dim = -1)[1].data.cpu()
+                    total_num += data.size(0)
+                    correct_num += (pred_lbl.squeeze() == lbl.data.cpu().squeeze()).sum()
+                    loss = criterion(opt, lbl)
+                accuracy = correct_num * 1.0 / total_num
+
+                logging.info('Validating [%d], Loss: %.4f, accuracy: %.4f', epoch,
+                                loss.data[0], accuracy)
+         
+                model.train()
+
+        if epoch % 10 == 0:
+            logging.info('Epoch [%d/%d], Loss: %.4f, accuracy: %5.4f'
+                          ,epoch, args.num_epochs, 
+                            loss.data[0], accuracy)
+                # Save the models
+            torch.save(model.state_dict(), 
+                os.path.join(args.model_path, 
+                        'model-%d.pkl' %(epoch+1)))
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', type=str, default='./models_lstm/' ,
+                        help='path for saving trained models')
+    # Model parameters
+    parser.add_argument('--data_dir', type = str, default = './data/utkinect/joints_processed_shrink_rm_center/')
+    parser.add_argument('--data_dir_test', type = str, default = './data/utkinect/joints_processed_shrink_rm_center_test/')
+    parser.add_argument('--input_size', type=int , default=45,
+                        help='dimension of input skeleton size(default 3d)')
+    parser.add_argument('--ds', type = str, default = 'UTKinect')
+    parser.add_argument('--seq_len', type=int , default=10, help = 'default length of the sequence for training.')
+    parser.add_argument('--hidden_size', type=int , default=128,
+                        help='dimension of lstm hidden states')
+    parser.add_argument('--num_layers', type=int , default=2,
+                        help='number of layers in lstm')
+    parser.add_argument('--num_class', type = int, default = 10, help = 'number of action classes')
+    parser.add_argument('--use_bias', action='store_true', help = 'use the bias or not in lstm.')
+    parser.add_argument('--dropout', type = float, default = 0.5)
+    parser.add_argument('--log_step', type = int, default = 1)
+    parser.add_argument('--eval_step', type = int, default = 1)
+    parser.add_argument('--save_step', type = int, default = 100)
+    parser.add_argument('--num_epochs', type=int, default=1000)
+    parser.add_argument('--batch_size', type=int, default=56)
+    parser.add_argument('--num_workers', type=int, default=1)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    args = parser.parse_args()
+    print(args)
+    main(args)
